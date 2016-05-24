f842cca5322583ecd831b3897f9cf00ad6c14b5e
Manu Cutillas <manucutillas@outlook.com>
2016-05-24 16:46:10 +0200

    First commit for this package

---
+++ README.md
@@ -0,0 +1,65 @@
+# Introduction
+[![MIT license](http://img.shields.io/badge/license-MIT-brightgreen.svg)](http://opensource.org/licenses/MIT)
+
+Special directives to show or hide items according to the size of the device screen in Angular 2 RC
+
+`angular2-responsive-directives` provides the following features:
+ - Directives detecting states according to standard measures in bootstrap: lg / md / sm / xs
+ - Directives that detect three states according to the type of device screens.
+ - Written for the latest release of Angular 2 in typescript.
+ 
+ # How to start
+ 
+ ```bash
+
+# 1. installing the package via npm 
+npm install a2-responsive-directives --save
+
+# 2. import the angular2-responsive-directives in your Angular 2 component
+
+# With Bootstrap Screen sizes  Directives
+import {LG,MD,SM,XS } from 'a2-responsive-directives';
+
+# With Devices Screen sizes Directives
+import {IsDesktop,IsMobile,IsTablet } from 'a2-responsive-directives';
+
+# 3. Assign directives for use in a component
+
+# With Bootstrap Screen sizes
+@Component({
+  selector: 'my-component',
+  template: `
+    <p *lg>I'll show you if I'm a desktop device.</p>
+    <p *md>I'll show you if I'm a tablet device.</p>
+    <p *sm>I'll show you if I'm a mobile device.</p>
+    <p *xs>I'll show you if I'm a mobile device.</p>
+  `,
+  directives: [LG,MD,SM,XS]
+})
+
+# With Devices Screen sizes
+@Component({
+  selector: 'my-component',
+  template: `
+    <p *isDesktop>I'll show you if I'm a desktop device.</p>
+    <p *isTablet>I'll show you if I'm a tablet device.</p>
+    <p *isMobile>I'll show you if I'm a mobile device.</p>
+  `,
+  directives: [IsDesktop,IsMobile,IsTablet ]
+})
+
+```
+
+
+
+# NEXT 
+Will work in the following features:
+
+- Detection device: browser type, operating system..etc
+- Detect portrait and landscape device status
+
+
+
+# License
+
+MIT 
---
+++ a2-responsive-directives.ts
@@ -0,0 +1,383 @@
+/**
+ * Responsive Detect States Directives for Angular 2
+ *
+ * @created_by Manu Cutillas
+ * @created_at May 23, 2016
+ * @updated_at May 24, 2016
+ * @version_1.0
+ * 
+ * Dependencies:
+ * @angular/core : "2.0.0-rc.1"
+ * rxjs: "5.0.0-beta.6"
+ *
+ * @more_info http://kalypso.agency
+ *            https://github.com/ManuCutillas
+ *            https://www.npmjs.com/~manucutillas
+ * 
+ * @description : First version of Responsive Detect State for Angular 2
+ *
+ */
+
+import {Injectable, Directive, Input, TemplateRef, ViewContainerRef, ElementRef} from '@angular/core';
+import 'rxjs/add/operator/share';
+import {Observable, Observer} from  'rxjs/Rx';
+
+export const RESPONSIVE_DEVICE_SIZES = {
+      lg: { min: 1200 },
+      md: { min: 992, max: 1199 },
+      sm: { min: 768, max: 991 },
+      xs: { max: 767 } };
+
+@Injectable()
+export class ResponsiveState {
+    private elementoObservar: Observable<any>;
+    private width: any;
+    
+    constructor() {
+       this.elementoObservar = Observable.fromEvent(window, 'resize').map(this.sizeOperations).share();
+    }
+
+    getDeviceSizeInitial(){
+        return this.sizeOperations();
+    }
+
+    private sizeOperations = (): any => {
+        this.width = () =>{ return window.innerWidth; } 
+        try {
+            if (RESPONSIVE_DEVICE_SIZES.lg.min < this.width) {
+                return 'lg';  
+            } else if (RESPONSIVE_DEVICE_SIZES.md.max > this.width && RESPONSIVE_DEVICE_SIZES.md.min < this.width) {
+                return 'md';   
+            } else if (RESPONSIVE_DEVICE_SIZES.sm.max > this.width && RESPONSIVE_DEVICE_SIZES.sm.min < this.width) {
+                return 'sm';   
+            } else if (RESPONSIVE_DEVICE_SIZES.xs.max > this.width) {
+                return 'xs';
+            }
+        } catch (error) {
+            //console.error('size operations error :', error);
+        }
+    }
+}
+
+/*
+ * DEVICES DIRECTIVES
+ * @Desktops / @Tablets / @Mobiles
+ */
+
+/*======== DESKTOPS STATES =========*/
+@Directive({
+    selector: '[isDesktop]',
+    providers: [ResponsiveState]
+})
+export class IsDesktop {
+    private sizeLG: string = 'lg';
+    private sizeMD: string = 'md';
+    private noRepeat: number = 0;
+    constructor(
+        private templateRef: TemplateRef<any>,
+        private viewContainer: ViewContainerRef,
+        private _responsiveState: ResponsiveState
+    ) { 
+        if(this.initalDeviceSize()){
+          this.viewContainer.createEmbeddedView(this.templateRef);
+          this.noRepeat = 1;   
+        }
+    }
+    @Input() set isDesktop(element: any) {
+
+        this._responsiveState.elementoObservar.subscribe((valor) => {
+            if (valor == this.sizeMD || valor == this.sizeLG) {
+                if (this.noRepeat == 0) {
+                    this.noRepeat = 1;
+                    this.viewContainer.createEmbeddedView(this.templateRef);
+                }
+            } else {
+                this.noRepeat = 0;
+                this.viewContainer.clear();
+            }
+
+        });
+    }
+    
+    initalDeviceSize() {
+        let initialDevice: any = this._responsiveState.getDeviceSizeInitial();
+        if (initialDevice == 'lg' || initialDevice == 'md') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+/*======== TABLETS STATES =========*/
+@Directive({
+    selector: '[isTablet]',
+    providers: [ResponsiveState]
+})
+export class IsTablet {
+    private sizeSM: string = 'sm';
+    private noRepeat: number = 0;
+    constructor(
+        private templateRef: TemplateRef<any>,
+        private viewContainer: ViewContainerRef,
+        private _responsiveState: ResponsiveState
+    ) { 
+        if(this.initalDeviceSize()){
+          this.viewContainer.createEmbeddedView(this.templateRef);
+          this.noRepeat = 1;   
+        }
+    }
+    @Input() set isDesktop(element: any) {
+
+        this._responsiveState.elementoObservar.subscribe((valor) => {
+            if (valor == this.sizeSM) {
+                if (this.noRepeat == 0) {
+                    this.noRepeat = 1;
+                    this.viewContainer.createEmbeddedView(this.templateRef);
+                }
+            } else {
+                this.noRepeat = 0;
+                this.viewContainer.clear();
+            }
+
+        });
+    }
+    
+    initalDeviceSize() {
+        let initialDevice: any = this._responsiveState.getDeviceSizeInitial();
+        if (initialDevice == 'sm') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+/*======== MOBILE STATES =========*/
+@Directive({
+    selector: '[isMobile]',
+    providers: [ResponsiveState]
+})
+export class IsMobile {
+    private sizeXS: string = 'xs';
+    private noRepeat: number = 0;
+    constructor(
+        private templateRef: TemplateRef<any>,
+        private viewContainer: ViewContainerRef,
+        private _responsiveState: ResponsiveState
+    ) { 
+        if(this.initalDeviceSize()){
+          this.viewContainer.createEmbeddedView(this.templateRef);
+          this.noRepeat = 1;   
+        }
+    }
+    @Input() set isDesktop(element: any) {
+
+        this._responsiveState.elementoObservar.subscribe((valor) => {
+            if (valor == this.sizeXS) {
+                if (this.noRepeat == 0) {
+                    this.noRepeat = 1;
+                    this.viewContainer.createEmbeddedView(this.templateRef);
+                }
+            } else {
+                this.noRepeat = 0;
+                this.viewContainer.clear();
+            }
+
+        });
+    }
+    
+    initalDeviceSize() {
+        let initialDevice: any = this._responsiveState.getDeviceSizeInitial();
+        if (initialDevice == 'xs') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+/*
+ *
+ * Bootstrap standard screen sizes directives
+ * LG / MD / SM / XS
+ */
+
+/*======== LG STATES =========*/
+@Directive({
+    selector: '[lg]',
+    providers: [ResponsiveState]
+})
+export class LG {
+    private state: string = 'lg';
+    private noRepeat: number = 0;
+    constructor(
+        private templateRef: TemplateRef<any>,
+        private viewContainer: ViewContainerRef,
+        private _responsiveState: ResponsiveState
+    ) { 
+        if(this.initalDeviceSize()){
+          this.viewContainer.createEmbeddedView(this.templateRef);
+          this.noRepeat = 1;   
+        }
+    }
+    @Input() set lg(element: any) {
+
+        this._responsiveState.elementoObservar.subscribe((valor) => {
+            if (valor == this.state) {
+                if (this.noRepeat == 0) {
+                    this.noRepeat = 1;
+                    this.viewContainer.createEmbeddedView(this.templateRef);
+                }
+            } else {
+                this.noRepeat = 0;
+                this.viewContainer.clear();
+            }
+
+        });
+    }
+    
+    initalDeviceSize() {
+        let initialDevice: any = this._responsiveState.getDeviceSizeInitial();
+        if (initialDevice == 'lg') {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
+
+/*======== MD STATES =========*/
+@Directive({
+    selector: '[md]',
+    providers: [ResponsiveState]
+})
+export class MD {
+    private state: string = 'md';
+    private noRepeat: number = 0;
+    constructor(
+        private templateRef: TemplateRef<any>,
+        private viewContainer: ViewContainerRef,
+        private _responsiveState: ResponsiveState
+    ) { 
+        if(this.initalDeviceSize()){
+          this.viewContainer.createEmbeddedView(this.templateRef);
+          this.noRepeat = 1;   
+        }
+    }
+    @Input() set md(element: any) {
+
+        this._responsiveState.elementoObservar.subscribe((valor) => {
+            if (valor == this.state) {
+                if (this.noRepeat == 0) {
+                    this.noRepeat = 1;
+                    this.viewContainer.createEmbeddedView(this.templateRef);
+                }
+            } else {
+                this.noRepeat = 0;
+                this.viewContainer.clear();
+            }
+        });
+    }
+    initalDeviceSize() {
+        let initialDevice: any = this._responsiveState.getDeviceSizeInitial();
+        if (initialDevice == 'md') {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
+
+/*======== SM STATES =========*/
+@Directive({
+    selector: '[sm]',
+    providers: [ResponsiveState]
+})
+export class SM {
+
+    private state: string = 'sm';
+    private noRepeat: number = 0;
+
+    constructor(
+        private templateRef: TemplateRef<any>,
+        private viewContainer: ViewContainerRef,
+        private _responsiveState: ResponsiveState
+    ) {
+        if(this.initalDeviceSize()){
+          this.viewContainer.createEmbeddedView(this.templateRef);
+          this.noRepeat = 1;   
+        }
+     }
+    @Input() set sm(element: any) {
+
+        this._responsiveState.elementoObservar.subscribe((valor) => {
+            if (valor == this.state) {
+                if (this.noRepeat == 0) {
+                    this.noRepeat = 1;
+                    this.viewContainer.createEmbeddedView(this.templateRef);
+                }
+            } else {
+                this.noRepeat = 0;
+                this.viewContainer.clear();
+            }
+
+        });
+    }
+    initalDeviceSize() {
+        let initialDevice: any = this._responsiveState.getDeviceSizeInitial();
+        if (initialDevice == 'sm') {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
+
+/*======== XS STATES =========*/
+@Directive({
+    selector: '[xs]',
+    providers: [ResponsiveState]
+})
+export class XS {
+    private state: string = 'xs';
+    private noRepeat: number = 0;
+    constructor(
+        private templateRef: TemplateRef<any>,
+        private viewContainer: ViewContainerRef,
+        private _responsiveState: ResponsiveState
+    ) {
+        if(this.initalDeviceSize()){
+          this.viewContainer.createEmbeddedView(this.templateRef);
+          this.noRepeat = 1;   
+        }
+     }
+    @Input() set xs(element: any) {
+        this._responsiveState.elementoObservar.subscribe((valor) => {
+            if (valor == this.state) {
+                if (this.noRepeat == 0) {
+                    this.noRepeat = 1;
+                    this.viewContainer.createEmbeddedView(this.templateRef);
+                }
+            } else {
+                this.noRepeat = 0;
+                this.viewContainer.clear();
+            }
+        });
+    }
+    initalDeviceSize() {
+        let initialDevice: any = this._responsiveState.getDeviceSizeInitial();
+        if (initialDevice == 'xs') {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
+
+
