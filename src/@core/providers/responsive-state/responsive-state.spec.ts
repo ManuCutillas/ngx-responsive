9c4956d83fba55a3f3478b6a3fb48a3db9f75173
Manu Cutillas <manucutillas@outlook.com>
2018-04-01 22:55:39 +0200

    First steps with unit testing

---
+++ src/@core/providers/responsive-state/responsive-state.spec.ts
@@ -0,0 +1,14 @@
+import { ResponsiveState } from './responsive-state';
+// constants specs
+
+describe('ResponsiveState', () => {
+
+  beforeEach(() => {
+    // create a new instance of the ResponsiveState
+  });
+
+  describe('getDevicePixelRatio', () => {
+    xit('should return the pixelRatio of the device screen', () => {});
+  });
+
+});
