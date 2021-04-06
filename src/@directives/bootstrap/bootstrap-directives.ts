7440dd4f718bb7035a4c93736e6caace040d51ea
Stefan Schmidt <s.schmidt@civit.de>
2021-04-06 15:05:42 +0200

    test

--- src/@directives/bootstrap/bootstrap-directives.ts
+++ src/@directives/bootstrap/bootstrap-directives.ts
@@ -3,141 +3,138 @@
  *  @description bootstrap directives
  *  @license MIT
  */
-import { Directive, Input, TemplateRef, ViewContainerRef, ChangeDetectorRef } from '@angular/core';
-import { PLATFORM_ID, Inject } from '@angular/core';
-
-import { ResponsiveState } from '../../@core/providers/responsive-state/responsive-state';
-import { RESPONSIVE_BASE } from '../../@core/providers/responsive-base/responsive-base';
+import { ChangeDetectorRef, Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 import { PlatformService } from '../../@core/providers/platform-service/platform.service';
+import { RESPONSIVE_BASE } from '../../@core/providers/responsive-base/responsive-base';
+import { ResponsiveState } from '../../@core/providers/responsive-state/responsive-state';
+
 @Directive({
-    selector: '[xl]'
+    selector: '[xl]',
 })
 export class XlDirective extends RESPONSIVE_BASE<any> {
-
     protected _state = 'xl';
     protected _showWhenTrue = true;
-    
-    constructor( templateRef: TemplateRef<any>,
-                 viewContainer: ViewContainerRef,
-                 _responsiveState: ResponsiveState,
-                 cd: ChangeDetectorRef,
-                 platformService: PlatformService
-    ) {
 
-        super ( templateRef, viewContainer, _responsiveState, cd, platformService );
+    constructor(
+        templateRef: TemplateRef<any>,
+        viewContainer: ViewContainerRef,
+        _responsiveState: ResponsiveState,
+        cd: ChangeDetectorRef,
+        platformService: PlatformService
+    ) {
+        super(templateRef, viewContainer, _responsiveState, cd, platformService);
         this.setGrid(this._state, 'bootstrap');
     }
 }
 
-@Directive(
-{
-    selector: '[lg]'
+@Directive({
+    selector: '[lg]',
 })
 export class LgDirective extends RESPONSIVE_BASE<any> {
-
     protected _state = 'lg';
     protected _showWhenTrue = true;
-    constructor( templateRef: TemplateRef<any>,
-                 viewContainer: ViewContainerRef,
-                 _responsiveState: ResponsiveState,
-                 cd: ChangeDetectorRef,
-                 platformService: PlatformService
+    constructor(
+        templateRef: TemplateRef<any>,
+        viewContainer: ViewContainerRef,
+        _responsiveState: ResponsiveState,
+        cd: ChangeDetectorRef,
+        platformService: PlatformService
     ) {
-        super( templateRef, viewContainer, _responsiveState, cd, platformService );
-        this.setGrid( this._state, 'bootstrap' );
+        super(templateRef, viewContainer, _responsiveState, cd, platformService);
+        this.setGrid(this._state, 'bootstrap');
+        console.log('LgDirective');
     }
 }
-@Directive(
-{
-    selector: '[md]'
+@Directive({
+    selector: '[md]',
 })
 export class MdDirective extends RESPONSIVE_BASE<any> {
     protected _state = 'md';
     protected _showWhenTrue = true;
-    
-    constructor( templateRef: TemplateRef<any>,
-                 viewContainer: ViewContainerRef,
-                 _responsiveState: ResponsiveState,
-                 cd: ChangeDetectorRef,
-                 platformService: PlatformService
+
+    constructor(
+        templateRef: TemplateRef<any>,
+        viewContainer: ViewContainerRef,
+        _responsiveState: ResponsiveState,
+        cd: ChangeDetectorRef,
+        platformService: PlatformService
     ) {
-        super( templateRef, viewContainer, _responsiveState, cd, platformService );
-        this.setGrid( this._state, 'bootstrap' );
-    }
-}
-
-@Directive({
-    selector: '[sm]'
-})
-export class SmDirective extends RESPONSIVE_BASE<any> {
-
-    protected _state = 'sm';
-    protected _showWhenTrue = true;
-
-    constructor( templateRef: TemplateRef<any>,
-                 viewContainer: ViewContainerRef,
-                 _responsiveState: ResponsiveState,
-                 cd: ChangeDetectorRef,
-                 platformService: PlatformService
-    ) {
-        super( templateRef, viewContainer, _responsiveState, cd, platformService );
-        this.setGrid( this._state, 'bootstrap' );
-    }
-}
-
-@Directive({
-    selector: '[xs]'
-})
-export class XsDirective extends RESPONSIVE_BASE<any> {
-
-    protected _state = 'xs';
-    protected _showWhenTrue = true;
-    
-    constructor( templateRef: TemplateRef<any>,
-                 viewContainer: ViewContainerRef,
-                 _responsiveState: ResponsiveState,
-                 cd: ChangeDetectorRef,
-                 platformService: PlatformService
-    ) {
-        super( templateRef, viewContainer, _responsiveState, cd, platformService );
+        super(templateRef, viewContainer, _responsiveState, cd, platformService);
         this.setGrid(this._state, 'bootstrap');
     }
 }
-@Directive(
-{
-    selector: '[showItBootstrap]'
+
+@Directive({
+    selector: '[sm]',
+})
+export class SmDirective extends RESPONSIVE_BASE<any> {
+    protected _state = 'sm';
+    protected _showWhenTrue = true;
+
+    constructor(
+        templateRef: TemplateRef<any>,
+        viewContainer: ViewContainerRef,
+        _responsiveState: ResponsiveState,
+        cd: ChangeDetectorRef,
+        platformService: PlatformService
+    ) {
+        super(templateRef, viewContainer, _responsiveState, cd, platformService);
+        this.setGrid(this._state, 'bootstrap');
+    }
+}
+
+@Directive({
+    selector: '[xs]',
+})
+export class XsDirective extends RESPONSIVE_BASE<any> {
+    protected _state = 'xs';
+    protected _showWhenTrue = true;
+
+    constructor(
+        templateRef: TemplateRef<any>,
+        viewContainer: ViewContainerRef,
+        _responsiveState: ResponsiveState,
+        cd: ChangeDetectorRef,
+        platformService: PlatformService
+    ) {
+        super(templateRef, viewContainer, _responsiveState, cd, platformService);
+        this.setGrid(this._state, 'bootstrap');
+    }
+}
+@Directive({
+    selector: '[showItBootstrap]',
 })
 export class ShowItBootstrapDirective extends RESPONSIVE_BASE<any> {
     protected _showWhenTrue = true;
-    @Input() set showItBootstrap( grid_state: string[] | string ) {
-        this.setGrid( grid_state, 'bootstrap' );
+    @Input() set showItBootstrap(grid_state: string[] | string) {
+        this.setGrid(grid_state, 'bootstrap');
     }
-    constructor( templateRef: TemplateRef<any>,
-                 viewContainer: ViewContainerRef,
-                 _responsiveState: ResponsiveState,
-                 cd: ChangeDetectorRef,
-                 platformService: PlatformService
+    constructor(
+        templateRef: TemplateRef<any>,
+        viewContainer: ViewContainerRef,
+        _responsiveState: ResponsiveState,
+        cd: ChangeDetectorRef,
+        platformService: PlatformService
     ) {
-        super( templateRef, viewContainer, _responsiveState, cd, platformService );
+        super(templateRef, viewContainer, _responsiveState, cd, platformService);
     }
 }
-@Directive(
-{
-    selector: '[hideItBootstrap]'
+@Directive({
+    selector: '[hideItBootstrap]',
 })
 export class HideItBootstrapDirective extends RESPONSIVE_BASE<any> {
-
     protected _showWhenTrue = false;
 
-    @Input() set hideItBootstrap( grid_state: string[] | string ) {
-        this.setGrid( grid_state, 'bootstrap' );
+    @Input() set hideItBootstrap(grid_state: string[] | string) {
+        this.setGrid(grid_state, 'bootstrap');
     }
-    constructor( templateRef: TemplateRef<any>,
-                 viewContainer: ViewContainerRef,
-                 _responsiveState: ResponsiveState,
-                 cd: ChangeDetectorRef,
-                 platformService: PlatformService
+    constructor(
+        templateRef: TemplateRef<any>,
+        viewContainer: ViewContainerRef,
+        _responsiveState: ResponsiveState,
+        cd: ChangeDetectorRef,
+        platformService: PlatformService
     ) {
-        super( templateRef, viewContainer, _responsiveState, cd, platformService );
+        super(templateRef, viewContainer, _responsiveState, cd, platformService);
     }
 }
